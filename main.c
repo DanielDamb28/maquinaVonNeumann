d26bdf91778231b7265bad7952e2628cd89bde1c
Daniel Bastos <91846323+DanielDamb28@users.noreply.github.com>
2023-03-17 11:23:39 -0300

    Adicionando dados na memoria e mostrando na tela

--- main.c
+++ main.c
@@ -31,8 +31,7 @@ void separaPosicaoDaMemoriaDoValorDeEntrada(char *valorDeEntrada, char *posicaoD
 }
 
 int converteHexaParaInt(char* valorEmHexa){
-    char end;
-    int posicaoEmDecimal = strtol(valorEmHexa, end, 16);
+    short int posicaoEmDecimal = strtol(valorEmHexa, NULL, 16);
     return posicaoEmDecimal;
 }
 
@@ -59,7 +58,7 @@ char separaDataOrInstructionDoValorDeEntrada(char* valorDeEntrada, int inicioPro
 
 void preencheTodasAsPosicoesDoVetorComVazio(char *vetor, int tamanho){
     for(int i = 0; i < tamanho; i++){
-        vetor[i] = 'a';
+        vetor[i] = '\0';
     }
 }
 
@@ -76,11 +75,11 @@ void separaPrimeiraInstrucaoDoValorDeEntrada(char *valorDeEntrada, char *primeir
     limpaEspacosEmBrancoAEsquerdaDaString(primeiraInstrucao);
 }
 
-void separaOperacaoDaInstrucao(char *primeiraInstrucao, char* op, int inicioEnderecoMemoria){
+void separaOperacaoDaInstrucao(char *instrucao, char* op, int inicioEnderecoMemoria){
     int k = 0;
     for(int i = 0; i < inicioEnderecoMemoria; i++){
-        if(primeiraInstrucao[i] != ' '){
-            op[k++] = primeiraInstrucao [i];
+        if(instrucao[i] != ' '){
+            op[k++] = instrucao [i];
         } else {
             break;
         }
@@ -107,34 +106,87 @@ void separaSegundaInstrucaoDoValorDeEntrada(char *valorDeEntrada, char *segundaI
     limpaEspacosEmBrancoAEsquerdaDaString(segundaInstrucao);
 }
 
+int retornaPosicaoDoFimDaPalavra(char *palavra){
+    int posicao = 0;
+    for(int i = 0; i < strlen(palavra); i++){
+        if(palavra[i] < 97 || palavra[i] > 122){
+            posicao = i;
+            break;
+        }
+    }
+}
+
 int separaValorASerGuardado(char *valorDeEntrada, int inicioProximaInstrucao){
     int contador = 0;
-    char valorASerGuardadoEmHexa[10] = "\0\0\0\0\0\0\0\0\0\0";
+    char valorASerGuardadoEmHexa[5] = "\0\0\0\0\0";
     for(int i = inicioProximaInstrucao; i < strlen(valorDeEntrada); i++){
             valorASerGuardadoEmHexa[contador++] = valorDeEntrada[i];
     }
     limpaEspacosEmBrancoAEsquerdaDaString(valorASerGuardadoEmHexa);
-    return converteHexaParaInt(valorASerGuardadoEmHexa);
+    int a = converteHexaParaInt(valorASerGuardadoEmHexa);
+    return a;
 }
 
-void executaLeituraDosDadosEGuardaNaMemoria(){
-    char valorDeEntrada[30];
+guardaInstrucaoNaMemoria(char *op, int enderecoDeMemoria, int posicaoNaMemoria, char *memoria){
+    char conjuntoDeIntrucoes[32][6];
+    strcpy(conjuntoDeIntrucoes[0], "hlt");strcpy(conjuntoDeIntrucoes[1], "nop");strcpy(conjuntoDeIntrucoes[2], "add");strcpy(conjuntoDeIntrucoes[3], "sub");
+    strcpy(conjuntoDeIntrucoes[4], "mul");strcpy(conjuntoDeIntrucoes[5], "div");strcpy(conjuntoDeIntrucoes[6], "cmp");strcpy(conjuntoDeIntrucoes[7], "xchg");
+    strcpy(conjuntoDeIntrucoes[8], "nad");strcpy(conjuntoDeIntrucoes[9], "or");strcpy(conjuntoDeIntrucoes[10], "xor");strcpy(conjuntoDeIntrucoes[11], "not");
+    strcpy(conjuntoDeIntrucoes[12], "je");strcpy(conjuntoDeIntrucoes[13], "jne");strcpy(conjuntoDeIntrucoes[14], "jl");strcpy(conjuntoDeIntrucoes[15], "jle");
+    strcpy(conjuntoDeIntrucoes[16], "jg");strcpy(conjuntoDeIntrucoes[17], "jge");strcpy(conjuntoDeIntrucoes[18], "jmp");strcpy(conjuntoDeIntrucoes[19], "lda");
+    strcpy(conjuntoDeIntrucoes[20], "ldb");strcpy(conjuntoDeIntrucoes[21], "sta");strcpy(conjuntoDeIntrucoes[22], "stb");strcpy(conjuntoDeIntrucoes[23], "ldrb");
+    strcpy(conjuntoDeIntrucoes[24], "movial");strcpy(conjuntoDeIntrucoes[25], "moviah");strcpy(conjuntoDeIntrucoes[26], "addia");strcpy(conjuntoDeIntrucoes[27], "subia");
+    strcpy(conjuntoDeIntrucoes[28], "mulia");strcpy(conjuntoDeIntrucoes[29], "divia");strcpy(conjuntoDeIntrucoes[30], "lsh");strcpy(conjuntoDeIntrucoes[31], "rsh");
 
-    char valorDaPosicaoDaMemoriaEmHexa[3];
-    int posicaoNaMemoria;
+    unsigned int dado = 0;
+    unsigned int parte1 = 0;
+    unsigned int parte2 = 0;
+    unsigned short int opInt = 0;
+    for(int i = 0; i < 32; i++){
+        if(op == ' ')
+            break;
+        if(!strcmp(op, conjuntoDeIntrucoes[i])){
+            opInt = i;
+            break;
+        }
+    }
+    dado = opInt;
+    dado = dado << 11;
+    dado = dado | enderecoDeMemoria;
+    parte1 = dado >> 8;
+    parte2 = dado << 8;
+    parte2 = parte2 >> 8;
+    memoria[posicaoNaMemoria] = parte1;
+    memoria[posicaoNaMemoria + 1] = parte2;
+
+
+
+
+
+}
+
+void executaLeituraDosDadosEGuardaNaMemoria(char *memoria, char *palavra){
+    char valorDeEntrada[30] = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+
+    char valorDaPosicaoDaMemoriaEmHexa[3] = "\0\0\0";
+    int posicaoNaMemoria = 0;
 
     int inicioProximaInstrucao = 0;
 
     char dataOrInstruction;
 
-    fgets(valorDeEntrada, 30, stdin);
+    for(int i = 0; i < strlen(palavra);i++){
+        valorDeEntrada[i] = palavra[i];
+    }
 
-    separaPosicaoDaMemoriaDoValorDeEntrada(valorDeEntrada, valorDaPosicaoDaMemoriaEmHexa);
+    separaPosicaoDaMemoriaDoValorDeEntrada(&valorDeEntrada, valorDaPosicaoDaMemoriaEmHexa);
     posicaoNaMemoria = converteHexaParaInt(valorDaPosicaoDaMemoriaEmHexa);
+
 
     inicioProximaInstrucao = retornaPosicaoDaProximaInstrucao(valorDeEntrada, 0, ';');
 
     dataOrInstruction = separaDataOrInstructionDoValorDeEntrada(valorDeEntrada, inicioProximaInstrucao);
+
 
     inicioProximaInstrucao = retornaPosicaoDaProximaInstrucao(valorDeEntrada, inicioProximaInstrucao, ';');
 
@@ -146,7 +198,7 @@ void executaLeituraDosDadosEGuardaNaMemoria(){
         int pEnderecoDeMemoria = 0;
         char pOp[7] = "\0\0\0\0\0\0\0";
 
-        char segundaInstrucao[15];
+        char segundaInstrucao[15] = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
         char sEnderecoDeMemoriaEmHexa[4] = "\0\0\0\0";
         int sEnderecoDeMemoria = 0;
         char sOp[7] = "\0\0\0\0\0\0\0";
@@ -163,7 +215,10 @@ void executaLeituraDosDadosEGuardaNaMemoria(){
         else{
             int inicioEnderecoMemoria = retornaPosicaoDaProximaInstrucao(primeiraInstrucao, 0, '/');
             separaOperacaoDaInstrucao(primeiraInstrucao, pOp, inicioEnderecoMemoria-1);
+            pEnderecoDeMemoria = 0;
         }
+        guardaInstrucaoNaMemoria(pOp, pEnderecoDeMemoria, posicaoNaMemoria, memoria);
+
         separaSegundaInstrucaoDoValorDeEntrada(valorDeEntrada, segundaInstrucao, inicioProximoComando);
         if(strchr(segundaInstrucao, ' ')){
             int inicioEnderecoMemoria = retornaPosicaoDaProximaInstrucao(segundaInstrucao, 0, ' ');
@@ -171,62 +226,83 @@ void executaLeituraDosDadosEGuardaNaMemoria(){
             separaEnderecoDaInstrucao(segundaInstrucao, sEnderecoDeMemoriaEmHexa, inicioEnderecoMemoria);
             sEnderecoDeMemoria = converteHexaParaInt(sEnderecoDeMemoriaEmHexa);
         }else{
-            int inicioEnderecoMemoria = strlen(segundaInstrucao) - 1 ;
-            printf("%i\n", inicioEnderecoMemoria);
+            int inicioEnderecoMemoria = retornaPosicaoDoFimDaPalavra(segundaInstrucao);
             separaOperacaoDaInstrucao(segundaInstrucao, sOp, inicioEnderecoMemoria);
+            sEnderecoDeMemoria = 0;
         }
-        printf("Primeira Instrucao:\n");
-        printf("%s\n", pOp);
-        printf("%i\n", pEnderecoDeMemoria);
-        printf("Segunda Instrucao: \n");
-        printf("%s\n", sOp);
-        printf("%i\n", sEnderecoDeMemoria);
-        imprimeEmBinario(sEnderecoDeMemoria);
+        guardaInstrucaoNaMemoria(sOp, sEnderecoDeMemoria, posicaoNaMemoria + 2, memoria);
+
     }
     else if(dataOrInstruction == 'd'){
 
-        unsigned short int valorASerGuardado;
-
+        unsigned short int valorASerGuardado = 0;
         valorASerGuardado = separaValorASerGuardado(valorDeEntrada, inicioProximaInstrucao);
-
-        printf("Valor a ser guardado: %i", valorASerGuardado);
-    }else{
-        executaLeituraDosDadosEGuardaNaMemoria();
+        guardaInstrucaoNaMemoria(' ', valorASerGuardado, posicaoNaMemoria, memoria);
     }
 }
 
 int main()
 {
-    char memoria[154];
-    for(int i = 0; i < 154; i++){
-        memoria[i] = 2;
-    }
+    unsigned char memoria[154];
     unsigned short int mbr, mar, ibr, imm, pc = 0;
     unsigned char ir;
     unsigned char equal, lower, greater;
     unsigned char lFlag, rFlag;
     unsigned short int aluA, aluB, aluT = 0;
 
-    char conjuntoDeIntrucoes[32][6];
-    strcpy(conjuntoDeIntrucoes[0], "hlt");strcpy(conjuntoDeIntrucoes[1], "nop");strcpy(conjuntoDeIntrucoes[2], "add");strcpy(conjuntoDeIntrucoes[3], "sub");
-    strcpy(conjuntoDeIntrucoes[4], "mul");strcpy(conjuntoDeIntrucoes[5], "div");strcpy(conjuntoDeIntrucoes[6], "cmp");strcpy(conjuntoDeIntrucoes[7], "xchg");
-    strcpy(conjuntoDeIntrucoes[8], "nad");strcpy(conjuntoDeIntrucoes[9], "or");strcpy(conjuntoDeIntrucoes[10], "xor");strcpy(conjuntoDeIntrucoes[11], "not");
-    strcpy(conjuntoDeIntrucoes[12], "je");strcpy(conjuntoDeIntrucoes[13], "jne");strcpy(conjuntoDeIntrucoes[14], "jl");strcpy(conjuntoDeIntrucoes[15], "jle");
-    strcpy(conjuntoDeIntrucoes[16], "jg");strcpy(conjuntoDeIntrucoes[17], "jge");strcpy(conjuntoDeIntrucoes[18], "jmp");strcpy(conjuntoDeIntrucoes[19], "lda");
-    strcpy(conjuntoDeIntrucoes[20], "ldb");strcpy(conjuntoDeIntrucoes[21], "sta");strcpy(conjuntoDeIntrucoes[22], "stb");strcpy(conjuntoDeIntrucoes[23], "ldrb");
-    strcpy(conjuntoDeIntrucoes[24], "movial");strcpy(conjuntoDeIntrucoes[25], "moviah");strcpy(conjuntoDeIntrucoes[26], "addia");strcpy(conjuntoDeIntrucoes[27], "subia");
-    strcpy(conjuntoDeIntrucoes[28], "mulia");strcpy(conjuntoDeIntrucoes[29], "divia");strcpy(conjuntoDeIntrucoes[30], "lsh");strcpy(conjuntoDeIntrucoes[31], "rsh");
-
-    executaLeituraDosDadosEGuardaNaMemoria();
-
     for(int i = 0; i < 154; i++){
-        if(memoria[i] =! '\0'){
-            memoria[i] = '\0';
+        if(memoria[i] =! 47){
+            memoria[i] = 47;
         }
     }
+
+    FILE *arquivo;
+    char texto[30];
+
+    arquivo = fopen("entrada.txt", "r");
+
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    int i = 0;
+    while (fgets(texto, 30, arquivo) != NULL) {
+        executaLeituraDosDadosEGuardaNaMemoria(memoria, texto);
+        i++;
+    }
+
+    fclose(arquivo);
+
+    printf("     ");
+
+    for(int i = 0;i < 16; i++){
+        printf(" %02x ", i);
+    }
+
     for(int i = 0; i < 154;i++){
 
-        printf("|%c|", memoria[i]);
+        if(i % 16 == 0){
+            printf("\n");
+            printf("    ---------------------------------------------------------------- ");
+            printf("\n");
+            printf(" %02x -", i);
+        }
+        printf("|%02x|",memoria[i]);
+        if(i % 16 == 15){
+            printf(" - %02x", i - 15);
+        }
+        if(i == 153){
+            printf("\n");
+            printf("    ---------------------------------------------------------------- ");
+            printf("\n");
+        }
+    }
+
+    printf("     ");
+
+    for(int i = 145;i < 161; i++){
+        printf(" %02x ", i - 145);
     }
 
     return 0;
